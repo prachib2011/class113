646146b2011e26ab0541663c3d9286f554a0558f
prachib2011 <89467902+prachib2011@users.noreply.github.com>
2022-01-27 14:56:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function preload(){
+}
+
+function setup(){
+canvas = createCanvas(640, 400);
+canvas.position(110, 250);
+video = createCapture(VIDEO);
+video.hide();
+tint_color = "";
+}
+
+function draw(){
+image(video, 0, 0, 640, 400);
+tint(tint_color);
+}
+
+function take_snapshot(){
+save('mlp.png');
+}
+
+function filter_tint(){
+    tint_color = document.getElementById("color_name").value;
+}
